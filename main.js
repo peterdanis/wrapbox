c22e07e304a55fda1356cec890dbade17cc16669
Peter Danis <peter.danis@outlook.com>
2017-11-20 16:23:33 +0100

    Fix hanging electron process

--- main.js
+++ main.js
@@ -54,6 +54,10 @@ app.on("window-all-closed", () => {
   }
 });
 
+app.on('before-quit', () => {
+  win.removeAllListeners('close');
+});
+
 app.on("activate", () => {
   if (win === null) {
     createWindow();
