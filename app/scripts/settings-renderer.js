1ecf8bf21622e78d9dce7690963df7351eedc54a
Peter Danis <peter.danis@outlook.com>
2018-01-10 17:28:18 +0100

    WIP on settings page

--- app/scripts/settings-renderer.js
+++ app/scripts/settings-renderer.js
@@ -2,10 +2,12 @@
 const settings = require("./settings");
 const ui = require("./ui");
 
-
 // Main function running all sub-tasks.
 function start() {
-
+  const resx = document.querySelector("#resx");
+  resx.value = settings.windowWidth;
+  const resy = document.querySelector("#resx");
+  resy.value = settings.windowHeight;
 }
 
 // Start the main function when the page is ready.
