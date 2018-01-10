0bc8f0964473cbe81836902a1d32ab6b1569d476
Peter Danis <peter.danis@outlook.com>
2018-01-10 17:50:17 +0100

    WIP on settings page

--- app/scripts/settings-renderer.js
+++ app/scripts/settings-renderer.js
@@ -4,10 +4,19 @@ const ui = require("./ui");
 
 // Main function running all sub-tasks.
 function start() {
-  const resx = document.querySelector("#resx");
-  resx.value = settings.windowWidth;
-  const resy = document.querySelector("#resx");
-  resy.value = settings.windowHeight;
+  const resX = document.querySelector("#resx");
+  resX.value = settings.windowWidth;
+
+  const resY = document.querySelector("#resx");
+  resY.value = settings.windowHeight;
+
+  const maximized = document.querySelector("#maximized");
+  maximized.checked = settings.startMaximized;
+
+  const windowButtons = document.querySelector("#windowButtons");
+  if (settings.windowButtonsPosition === "right") {
+    windowButtons.checked = true;
+  }
 }
 
 // Start the main function when the page is ready.
