b3056ca2ec705f3b99030e4db9511b7b05c731da
Peter Danis <peter.danis@outlook.com>
2018-02-14 14:44:47 +0100

    Separate update functionality

--- app/app.js
+++ app/app.js
@@ -16,7 +16,6 @@ File log locations:
 */
 
 // Require rest of the dependencies
-const { autoUpdater } = require("electron-updater");
 const path = require("path");
 const url = require("url");
 const utils = require("./scripts/utils");
@@ -94,31 +93,3 @@ app.on("activate", () => {
     createWindow();
   }
 });
-
-autoUpdater.logger = null;
-
-autoUpdater.on("checking-for-update", () => {
-  log.info("Checking for update...");
-});
-
-autoUpdater.on("update-available", (info) => {
-  log.info(`Update available.\n${info}`);
-});
-
-autoUpdater.on("update-not-available", (info) => {
-  log.info(`Update not available.\n${info}`);
-});
-
-autoUpdater.on("update-downloaded", (info) => {
-  log.info(`Update downloaded.\n${info}`);
-});
-
-autoUpdater.on("error", (err) => {
-  let message = err.message.slice(0, 600);
-  if (err.message.length > 600) {
-    message += "...\n( error truncated)";
-  }
-  log.error(`Error in auto-updater:\n${message}`);
-});
-
-autoUpdater.checkForUpdates();
--- app/scripts/update.js
+++ app/scripts/update.js
@@ -2,7 +2,33 @@ const { app } = require("electron").remote; // eslint-disable-line
 const { autoUpdater } = require("electron-updater");
 const log = require("electron-log");
 
-autoUpdater.on("checking-for-update", log.info());
+autoUpdater.logger = null;
+
+autoUpdater.on("checking-for-update", () => {
+  log.info("Checking for update...");
+});
+
+autoUpdater.on("update-available", (info) => {
+  log.info(`Update available.\n${info}`);
+});
+
+autoUpdater.on("update-not-available", (info) => {
+  log.info(`Update not available.\n${info}`);
+});
+
+autoUpdater.on("update-downloaded", (info) => {
+  log.info(`Update downloaded.\n${info}`);
+});
+
+autoUpdater.on("error", (err) => {
+  let message = err.message.slice(0, 600);
+  if (err.message.length > 600) {
+    message += "...\n( error truncated)";
+  }
+  log.error(`Error in auto-updater:\n${message}`);
+});
+
+autoUpdater.checkForUpdates();
 
 // app.relaunch();
 // app.quit();
