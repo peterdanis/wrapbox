505553fe2eddb7b82677c76504c05cfdc14fd496
Peter Danis <peter.danis@outlook.com>
2018-02-19 22:54:31 +0100

    chore: tests

--- tests/unit/add-navigation-buttons.test.js
+++ tests/unit/add-navigation-buttons.test.js
@@ -3,12 +3,23 @@ const EventEmitter = require("events");
 
 const testEmitter = new EventEmitter();
 
+function getDiv0() {
+  return document.querySelector("#div0");
+}
+
+function getNbdiv0() {
+  return document.querySelector("#nbdiv0");
+}
+
 function createTestDocument() {
   for (let i = 0; i < 2; i++) {
     const div = document.createElement("div");
     document.querySelector("body").appendChild(div);
     div.setAttribute("id", `div${i}`);
   }
+  const activeDiv = document.createElement("div");
+  activeDiv.classList.add("active");
+  getDiv0().appendChild(activeDiv);
 }
 
 beforeAll(() => {
@@ -17,17 +28,24 @@ beforeAll(() => {
 
 describe("AddNavButtons function", () => {
   test("creates navbuttons section only in defined element", () => {
-    addNavButtons("#div0", testEmitter, 0);
-    expect(document.querySelector("#nbdiv0").parentNode.id).toBe("div0");
+    addNavButtons(getDiv0(), testEmitter, 0);
+    expect(getNbdiv0().parentNode.id).toBe("div0");
     expect(document.querySelector("#div1").childElementCount).toBe(0);
   });
 
   test("navbuttons are hidden by default", () => {
-    expect(document.querySelector("#nbdiv0").classList.contains("invisible")).toBeTruthy();
+    expect(getNbdiv0().classList.contains("invisible")).toBeTruthy();
   });
 
   test("navbuttons are visible if parent node is 'active'", () => {
-    expect(document.querySelector("#nbdiv0").classList.contains("invisible")).toBeTruthy();
+    testEmitter.emit("showNavButtons", getDiv0());
+    expect(getNbdiv0().classList.contains("invisible")).toBeFalsy();
+  });
+
+  test("navbuttons are hidden", () => {
+    testEmitter.emit("hideNavButtons");
+    testEmitter.emit("showNavButtons", {});
+    expect(getNbdiv0().classList.contains("invisible")).toBeTruthy();
   });
 
   test("adds 4 buttons to navbuttons element", () => {
