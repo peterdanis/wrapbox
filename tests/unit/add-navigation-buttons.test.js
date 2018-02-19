9c4c8459c68a933d15acf0b14fbd5c3b4e37e9b2
Peter Danis <peter.danis@outlook.com>
2018-02-20 00:10:23 +0100

    chore: add tests

--- tests/unit/add-navigation-buttons.test.js
+++ tests/unit/add-navigation-buttons.test.js
@@ -4,7 +4,7 @@ const EventEmitter = require("events");
 const testEmitter = new EventEmitter();
 
 function getDiv0() {
-  return document.querySelector("#div0");
+  return document.querySelector("#wbdiv0");
 }
 
 function getNbdiv0() {
@@ -15,8 +15,9 @@ function createTestDocument() {
   for (let i = 0; i < 2; i++) {
     const div = document.createElement("div");
     document.querySelector("body").appendChild(div);
-    div.setAttribute("id", `div${i}`);
+    div.setAttribute("id", `wbdiv${i}`);
   }
+
   const activeDiv = document.createElement("div");
   activeDiv.classList.add("active");
   getDiv0().appendChild(activeDiv);
@@ -29,8 +30,8 @@ beforeAll(() => {
 describe("AddNavButtons function", () => {
   test("creates navbuttons section only in defined element", () => {
     addNavButtons(getDiv0(), testEmitter, 0);
-    expect(getNbdiv0().parentNode.id).toBe("div0");
-    expect(document.querySelector("#div1").childElementCount).toBe(0);
+    expect(getNbdiv0().parentNode.id).toBe("wbdiv0");
+    expect(document.querySelector("#wbdiv1").childElementCount).toBe(0);
   });
 
   test("navbuttons are hidden by default", () => {
@@ -49,10 +50,34 @@ describe("AddNavButtons function", () => {
   });
 
   test("adds 4 buttons to navbuttons element", () => {
-    const buttons = document.querySelector("#nbdiv0").childNodes;
+    const buttons = getNbdiv0().childNodes;
     expect(buttons.length).toBe(4);
     for (let i = 0; i < buttons.length; i++) {
       expect(buttons[i]).toMatchSnapshot();
     }
+  });
+
+  test("nabuttons maps to webview methods", () => {
+    const webview0 = document.createElement("div");
+    webview0.setAttribute("id", "webview0");
+    webview0.goBack = jest.fn(() => {});
+    webview0.goToIndex = jest.fn(() => {});
+    webview0.reload = jest.fn(() => {});
+    webview0.goForward = jest.fn(() => {});
+    document.body.appendChild(webview0);
+
+    getNbdiv0().childNodes[0].innerText = "navigate_before";
+    getNbdiv0().childNodes[1].innerText = "home";
+    getNbdiv0().childNodes[2].innerText = "refresh";
+    getNbdiv0().childNodes[3].innerText = "navigate_next";
+
+    getNbdiv0().childNodes.forEach((e) => {
+      e.click();
+    });
+
+    expect(webview0.goBack).toHaveBeenCalled();
+    expect(webview0.goToIndex).toHaveBeenCalledWith(0);
+    expect(webview0.reload).toHaveBeenCalled();
+    expect(webview0.goForward).toHaveBeenCalled();
   });
 });
