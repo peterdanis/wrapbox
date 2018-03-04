fa59331a1ad9924f60ef9961dcedf860b69cc392
Peter Danis <peter.danis@outlook.com>
2018-03-04 21:37:37 +0100

    Fix lint err

--- tests/integration/app.test.js
+++ tests/integration/app.test.js
@@ -4,15 +4,15 @@ const path = require("path");
 
 expect.extend({ toMatchImageSnapshot });
 
-const electronBin = (() => {
-  if (process.platform === "win32") {
-    return "electron.exe";
-  }
-  if (process.platform === "darwin") {
-    return "electron.app";
-  }
-  return "electron";
-})();
+// const electronBin = (() => {
+//   if (process.platform === "win32") {
+//     return "electron.exe";
+//   }
+//   if (process.platform === "darwin") {
+//     return "electron.app";
+//   }
+//   return "electron";
+// })();
 
 const electronPath = path.join(__dirname, "..", "..", "node_modules", ".bin", "electron");
 const appPath = [path.join(__dirname, "..", "..")];
