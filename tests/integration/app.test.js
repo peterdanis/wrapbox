85eccd0d9362cf33ab7da85753a1850e6aeb2ca4
Peter Danis <peter.danis@outlook.com>
2018-03-04 19:59:31 +0100

    Add app test

---
+++ tests/integration/app.test.js
@@ -0,0 +1,44 @@
+const { Application } = require("spectron");
+const { toMatchImageSnapshot } = require("jest-image-snapshot");
+const path = require("path");
+
+expect.extend({ toMatchImageSnapshot });
+
+const electronPath = path.join(
+  __dirname,
+  "..",
+  "..",
+  "node_modules",
+  "electron",
+  "dist",
+  "electron"
+);
+const appPath = [path.join(__dirname, "..", "..")];
+const app = new Application({
+  path: electronPath,
+  args: appPath,
+});
+
+describe("App", () => {
+  test(
+    "starts",
+    async () => {
+      await app.start();
+      const isVisible = await app.browserWindow.isVisible();
+      await app.stop();
+      expect(isVisible).toBe(true);
+    },
+    10000
+  );
+  // test(
+  //   "image matches",
+  //   async () => {
+  //     await app.start();
+  //     app.client.pause(1000);
+  //     const image = await app.browserWindow.capturePage();
+  //     await app.stop();
+  //     expect(image).toMatchImageSnapshot();
+  //   },
+  //   10000
+  // );
+});
