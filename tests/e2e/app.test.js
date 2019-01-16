be7a6f098b330623fb99ae07ff3c42d5458e3272
Peter Danis <peter.danis@outlook.com>
2019-01-16 21:36:22 +0100

    Replace spectron with puppeteer

--- tests/e2e/app.test.js
+++ tests/e2e/app.test.js
@@ -1,115 +1,42 @@
-const { Application } = require("spectron");
 const { toMatchImageSnapshot } = require("jest-image-snapshot");
 const electron = require("electron");
-const path = require("path");
+// const path = require("path");
+const puppeteer = require("puppeteer");
 
 const delay = ms => new Promise((resolve) => {
   setTimeout(() => {
     resolve();
   }, ms);
 });
+
 let app;
+let page;
 
 beforeAll(async () => {
   expect.extend({ toMatchImageSnapshot });
-  const preloadFile = path.posix.join(__dirname, "test-preload.js");
-  app = new Application({
-    path: electron,
-    args: ["-r", preloadFile, "."],
-    startTimeout: 30000,
+  // const preloadFile = path.posix.join(__dirname, "test-preload.js");
+  app = await puppeteer.launch({
+    executablePath: electron,
+    args: ["."],
+    headless: false,
   });
-  await app.start();
+  const pages = await app.pages();
+  [page] = pages;
+  page.setViewport({ width: 1200, height: 700 });
 }, 30000);
 
 afterAll(async () => {
-  await app.stop();
-  await delay(5000);
+  await app.close();
 }, 30000);
 
 describe("App", () => {
-  test(
-    "starts",
-    async () => {
-      const isVisible = await app.browserWindow.isVisible();
-
-      expect(isVisible).toBe(true);
-    },
-    10000
-  );
-
-  // TODO: WIP tests
-  /*
-  test(
-    "maximizes",
-    async () => {
-      if (process.platform === "darwin") {
-        await app.browserWindow.maximize();
-      } else {
-        await app.client.leftClick("#maximize");
-      }
-      await app.client.waitUntil(
-        async () => (await app.browserWindow.isMaximized()) === true,
-        5000,
-        "",
-        100
-      );
-      const isMaximized = await app.browserWindow.isMaximized();
-
-      expect(isMaximized).toBe(true);
-    },
-    10000
-  );
-  test(
-    "unmaximizes",
-    async () => {
-      if (process.platform === "darwin") {
-        await app.browserWindow.maximize();
-      } else {
-        await app.client.leftClick("#maximize");
-      }
-      await app.client.waitUntil(
-        async () => (await app.browserWindow.isMaximized()) === false,
-        5000,
-        "",
-        100
-      );
-      const isMaximized = await app.browserWindow.isMaximized();
-
-      expect(isMaximized).toBe(false);
-    },
-    10000
-  );
-  test(
-    "minimizes",
-    async () => {
-      if (process.platform === "darwin") {
-        await app.browserWindow.minimize();
-      } else {
-        await app.client.leftClick("#minimize");
-      }
-      await app.client.waitUntil(
-        async () => (await app.browserWindow.isMinimized()) === true,
-        5000,
-        "",
-        100
-      );
-      const isMinimized = await app.browserWindow.isMinimized();
-      await app.stop();
-
-      expect(isMinimized).toBe(true);
-    },
-    10000
-  );
-  test(
-    "image matches",
-    async () => {
-      await app.start();
-      app.client.pause(1000);
-      const image = await app.browserWindow.capturePage();
-      await app.stop();
-      expect(image).toMatchImageSnapshot();
-    },
-    10000
-  );
-  */
+  test("starts", async () => {
+    await delay(5000);
+    const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot({
+      customDiffConfig: { threshold: 0.1 },
+      failureThreshold: "0.5",
+      failureThresholdType: "percent",
+    });
+  }, 10000);
 });
