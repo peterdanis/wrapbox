be9f7e70dc01027dab0060a3b18b5d5cddf142c6
Peter Danis <peter.danis@outlook.com>
2019-06-14 13:36:50 +0200

    Prepare electron file

--- src/electron/electron.ts
+++ src/electron/electron.ts
@@ -1,26 +1,83 @@
-import electron from "electron"; // eslint-disable-line import/no-extraneous-dependencies
+import { app, BrowserWindow } from "electron"; // eslint-disable-line import/no-extraneous-dependencies
 import path from "path";
+import log from "electron-log";
+import url from "url";
 
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+// Change log level for file log to info and log app start
+log.transports.file.level = "info";
+log.info("App start");
+log.info(`Version: ${app.getVersion()}`);
+log.info(`Platform: ${process.platform}`);
+log.info(`Arch: ${process.arch}`);
+log.info(`Log file location: ${log.transports.file.file}`);
+/*
+File log locations:
+  on Linux: ~/.config/<app name>/log.log
+  on OS X: ~/Library/Logs/<app name>/log.log
+  on Windows: %USERPROFILE%\AppData\Roaming\<app name>\log.log
+*/
 
 let mainWindow: Electron.BrowserWindow | null;
 
-const createWindow: Function = (): void => {
+const appQuit = (): void => {
+  if (process.platform !== "darwin") {
+    log.info("App quit");
+    app.quit();
+  }
+};
+
+const createWindow = (): void => {
   mainWindow = new BrowserWindow({ width: 900, height: 680 });
   mainWindow.loadURL(
     app.isPackaged
-      ? `file://${path.join(__dirname, "../build/index.html")}`
-      : "http://localhost:3000"
+      ? url.format({
+          pathname: path.join(__dirname, "index.html"),
+          protocol: "file:",
+          slashes: true,
+        })
+      : "http://localhost:3000",
   );
-  mainWindow.on("closed", () => (mainWindow = null));
+
+  // TODO: Add after setting up storage
+  // if (utils.settings.startMaximized) {
+  //   mainWindow.maximize();
+  // }
+
+  // Window listeners
+  mainWindow.once("ready-to-show", () => {
+    if (mainWindow) {
+      mainWindow.show();
+    }
+  });
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+    log.info("Window closed");
+    appQuit();
+  });
 };
+
+// Fix for Win10 notifications
+app.setAppUserModelId("com.peterdanis.wrapbox");
+
+// App listeners
+app.on("ready", () => {
+  createWindow();
+  if (!process.env.PORTABLE_EXECUTABLE_DIR) {
+    // TODO: update after setting up updating module
+    // update.checkForUpdatesAndNotify();
+  }
+});
 
 // @ts-ignore
 app.on("ready", createWindow);
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
   }
+});
+
+app.on("window-all-closed", () => {
+  log.info("All windows closed");
 });
