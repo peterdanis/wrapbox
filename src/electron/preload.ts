a3b21939b216b74706df22fdb9ef94dce4a77302
Peter Danis <peter.danis@outlook.com>
2019-06-24 14:45:29 +0200

    Remove extraenous comment

--- src/electron/preload.ts
+++ src/electron/preload.ts
@@ -1,4 +1,4 @@
-import { ipcRenderer } from "electron"; // eslint-disable-line import/no-extraneous-dependencies
+import { ipcRenderer } from "electron";
 
 // @ts-ignore
 window.ipcRenderer = ipcRenderer;
