e4fdfa5c23e6cdc4cda48720fcbee240ba92bbea
Peter Danis <peter.danis@outlook.com>
2019-08-01 17:06:41 +0200

    Add roboto font

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
-import "./index.css";
+import "typeface-roboto"; // eslint-disable-line import/no-extraneous-dependencies
 import App from "./components/App";
 import { GlobalState } from "./components/GlobalState";
 import React from "react";
 import ReactDOM from "react-dom";
+
+document.body.style.margin = "0";
+document.body.style.fontFamily = "Roboto";
 
 ReactDOM.render(
   <GlobalState>
