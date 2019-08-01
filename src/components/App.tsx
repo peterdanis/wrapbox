71cd3572233c2e485fbc27c71b2c97bf13ac6db7
Peter Danis <peter.danis@outlook.com>
2019-08-01 17:16:20 +0200

    Get rid of css

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,28 @@
 import React, { Fragment } from "react";
+import { styled } from "@material-ui/styles";
 import TopBar from "./TopBar";
+
+const Content = styled("div")({
+  webkitAppRegion: "no-drag",
+  // position: fixed;
+  // top: 32px;
+  // width: 100%;
+  // height: calc(100% - 32px);
+});
+
+const Webview = styled("webview")({
+  height: "100%",
+  position: "absolute",
+  width: "100%",
+});
 
 export default function App(): React.ReactElement {
   return (
     <Fragment>
       <TopBar />
-      <div id="content">
-        <webview className="webview" />
-      </div>
+      <Content>
+        <Webview />
+      </Content>
     </Fragment>
   );
 }
